b722b9563b93e4e7eefb71d7086a9190aae881a0
unknown <sdgerbich@gmail.com>
2020-04-16 08:53:59 -0400

    hw

---
+++ codecamp.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('getMessage').onclick = function(){
+      // Add your code below this line
+  document.getElementsByClassName("message")[0].textContent= "Here is the message";
+
+      // Add your code above this line
+    }
+  });
